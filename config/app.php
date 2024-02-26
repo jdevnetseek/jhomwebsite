ec16ef9234bf673729f911d05fb9de02d12a8a4b
jhomignacio08@gmail.com <jhomignacio08@gmail.com>
2024-02-27 07:00:03 +0800

    added JWT

--- config/app.php
+++ config/app.php
@@ -161,6 +161,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        // JWT
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
 
         /*
          * Package Service Providers...
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', [App\Http\Controllers\AuthController::class, 'login']);
+    // Route::get('users', [App\Http\Controllers\Api\UserController::class, 'index']);
+
+
+});
